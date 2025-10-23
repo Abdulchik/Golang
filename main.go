fac196369ab500d7f0d699f5666bf22fc139f964
Abdullatif <abdul_ghaniev@bk.ru>
2025-10-23 21:39:44 +0500

    edition1

--- main.go
+++ main.go
@@ -5,28 +5,47 @@ import (
 )
 
 func main() {
-	var width, length int
-	fmt.Print("Введите ширину: ")
+label:
+	var width int
+	fmt.Print("Введите размер доски: ")
 	fmt.Scan(&width)
-	fmt.Print("Введите длину: ")
-	fmt.Scan(&length)
-	if width == length {
-		for i := 0; i < length; i++ {
 
-			if i%2 == 0 {
-				for j := 0; j < width; j++ {
-					fmt.Print("# ")
+	if width < 100 {
+		if width >= 0 {
+			if width%1 == 0 {
+				if width%2 == 0 {
+					for i := 0; i < width; i++ {
+
+						if i%2 == 0 {
+							for j := 0; j < width/2; j++ {
+								fmt.Print("# ")
+							}
+						} else {
+							for j := 0; j < width/2; j++ {
+								fmt.Print(" #")
+							}
+						}
+						fmt.Println()
+					}
+				} else {
+					fmt.Print("Ошибка: нечетные числа не поддерживаются.\n")
+					fmt.Print("Введите размеры заново.\n")
+					goto label
 				}
 			} else {
-				for j := 0; j < width; j++ {
-					fmt.Print(" #")
-				}
+				fmt.Print("Ошибка: дробные числа не поддерживаются.\n")
+				fmt.Print("Введите размеры заново.\n")
+				goto label
 			}
-			fmt.Println()
+		} else {
+			fmt.Print("Ошибка: отрицательные или равные 0 числа не поддерживаются.\n")
+			fmt.Print("Введите размеры заново.\n")
+			goto label
 		}
 
 	} else {
-
-		fmt.Print("Длина и ширина должны быть равны для создания шахматной доски.")
+		fmt.Print("Ошибка: число должно быть меньше 100.\n")
+		fmt.Print("Введите размеры заново.\n")
+		goto label
 	}
 }
