6fc5fa62a4b5297cc9305cdfce6a5a1d18d35bfc
Abdullatif <abdul_ghaniev@bk.ru>
2025-10-23 14:56:46 +0500

    Chess

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 7
+	var b int = 7
+
+	if a%2 == 0 {
+		for i := 0; i < a; i++ {
+			fmt.Print("\n")
+			if i%2 == 0 {
+				for o := 0; o < b; o++ {
+					fmt.Print("# ")
+
+				}
+			} else {
+				for j := 0; j < b-1; j++ {
+					fmt.Print(" #")
+				}
+			}
+		}
+
+	} else {
+		fmt.Print("Не четное число")
+	}
+}
